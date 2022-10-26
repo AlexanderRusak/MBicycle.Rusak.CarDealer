1f1781aaba2c5b31798043d5f707706daaa25437
AlexanderRusak <alexander.rusak@mbicycle.com>
2022-10-26 13:53:09 +0600

    add business layer

---
+++ src/CarDealer.BusinessLogic/AllCarsService.cs
@@ -0,0 +1,26 @@
+﻿using CarDealer.BusinessLogic.Dtos;
+using CarDealer.DataAccess.Model.Repositories.Interfaces;
+
+namespace CarDealer.BusinessLogic
+{
+    internal class AllCarsService : IAllCarsService
+    {
+        private readonly ICarRepository _carRepository;
+
+        public AllCarsService(ICarRepository carRepository)
+        {
+            _carRepository = carRepository;
+        }
+        public IEnumerable<CarDto> GetAllCars()
+        {
+            var result = _carRepository.Get().Select(car => new CarDto
+            {
+                Id = car.Id,
+                Brand = car.Brand.Name,
+                Model = car.Model,
+                Color = car.Color.Name
+            });
+            return result;
+        }
+    }
+}
